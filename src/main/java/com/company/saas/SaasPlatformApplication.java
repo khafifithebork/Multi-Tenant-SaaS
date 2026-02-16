3cd33aefe76b38f8d499b3eaa8a588469b9011b2
Ayman Khafifi <khafifiayman318@gmail.com>
2026-02-16 20:28:05 +0000

    Create SaasPlatformApplication.java

---
+++ src/main/java/com/company/saas/SaasPlatformApplication.java
@@ -0,0 +1,9 @@
+package com.company.saas;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SaasPlatformApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SaasPlatformApplication.class, args);
+    }
+}
